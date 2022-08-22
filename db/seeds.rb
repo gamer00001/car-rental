8f8680789c8e9b23a232d62554b0bf7a7cacb0fa
Faizan Haider <hfaizan53@gmail.com>
2022-08-22 22:00:21 +0500

    added seed file and dot-env gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+AdminUser.create!(email: 'admin@gmail.com', password: 'password', password_confirmation: 'password')
+Category.create!(name: "Prestige & Business cars")
+Category.create!(name: "SUV cars")
+Category.create!(name: "Sports  & Convertible cars")
