34f2914ba3c8cf53aea37f3225cd8ceafafe2bc9
Faizan Haider <hfaizan53@gmail.com>
2022-09-21 23:44:46 +0500

    UI fixes on admin and users site

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
   resources :cars
   resources :categorys
   resources :bookings
-  get "/admin", to: redirect("/")
+  get "/admin", to: "cars#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
