fe4ee4a636f0af6c820e75965baf49ee908dd0a4
Faizan Haider <hfaizan53@gmail.com>
2022-09-29 04:17:27 +0500

    fixes errors

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,6 +1,6 @@
 class Photo < ApplicationRecord
-    belongs_to :imageable
-    has_attached_file :photos, :styles => { :medium => "238x238>", :thumb => "100x100>"}
-    validates_attachment :image, content_type: { content_type:     ["image/jpg", "image/jpeg", "image/png"] }
+    # belongs_to :imageable
+    # has_attached_file :photos, :styles => { :medium => "238x238>", :thumb => "100x100>"}
+    # validates_attachment :image, content_type: { content_type:     ["image/jpg", "image/jpeg", "image/png"] }
 
 end
